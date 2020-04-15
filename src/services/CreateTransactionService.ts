bdb3232aab1fef701b9dfee3129dc75c78534cf1
AndersonDaSilvaOSF <43174799+AndersonDaSilvaOSF@users.noreply.github.com>
2020-04-15 17:35:30 -0400

    Final solution

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,38 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    if (['income', 'outcome'].indexOf(type) === -1) {
+      throw Error('Invalid type of transaction');
+    }
+
+    if (type === 'outcome' && value > balance.total) {
+      throw Error("You don't have balance to do this transaction");
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
